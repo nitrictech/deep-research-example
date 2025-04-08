9b5c14eab5babd75f701aca5e2cd17453a241d65
Tim Holm <tim.holm@nitric.io>
2025-04-09 09:59:54 +1000

    fix exports.

--- prompts/index.ts
+++ prompts/index.ts
@@ -1,5 +1,4 @@
 import query from "./query.ts";
 import summarize from "./summarizer.ts";
 import reflect from "./reflect.ts";
-import converter from "./converter.ts";
-export { query, summarize, reflect, converter };
+export { query, summarize, reflect };
